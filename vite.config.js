666a6998b725733e9d72cfe993b366bd8729e3bf
shravanrbangera <shravanshravan36327@gmail.com>
2026-08-16 17:17:22 +0530

    Set relative ./ asset paths for universal mobile and desktop compatibility across all hosting platforms

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/AQUASAGA-2026/',
+  base: './',
   server: {
     port: 3000,
     host: true
